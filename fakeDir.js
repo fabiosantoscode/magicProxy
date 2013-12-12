dd57afb762470112fb9ba8b20234cd8329243df6
Fábio Santos <fabio-s-santos@telecom.pt>
2013-12-12 00:02:26 +0000

    index files for directories, in fakeDir

--- fakeDir.js
+++ fakeDir.js
@@ -24,29 +24,59 @@ var fs = require('fs'),
 
 function fakeDir(req, res, plugin) {
     var remoteUrl = url.parse(req.url)
-    return (plugin.config.fakeDir || [])
-        .filter(function (fakeDir) {
-            return req.url.indexOf(path.dirname(fakeDir.url)) === 0
-        })
-        .some(function (fakeDir) {
-            var relativePath = path.relative(fakeDir.url, req.url)
-            var absPath = path.join(fakeDir.dir, relativePath)
-            if (!fakeDir.contentType) {
-                fakeDir.contentType = mime.lookup(absPath) || 'text/plain';
-            }
-            res.setHeader('Content-Type', fakeDir.contentType)
-            fs.readFile(absPath, function (err, data) {
-                if (err) {
-                    res.setHeader('Content-Type', 'text/plain')
-                    res.statusCode = 404
-                    res.write('MagicProxy fakeDir IO error');
-                    console.error(err);
-                    res.write(err.toString());
-                    return res.end()
-                }
-                res.end(data)
-            })
-            return true
-        })
+    var operations = plugin.config.fakeDir || []
+
+    var fakeDir = findOp(req, operations)
+
+    if (!fakeDir) {
+        return false;
+    }
+
+    var relativePath = path.relative(fakeDir.url, req.url)
+    var absPath = path.join(fakeDir.dir, relativePath)
+
+    if (!fakeDir.contentType) {
+        fakeDir.contentType = mime.lookup(absPath) || 'text/plain';
+    }
+
+    respondWith(fakeDir, res, absPath);
+
+    return true;
 }
 
+function findOp(req, operations) {
+    for (var i = 0, len = operations.length; i < len; i++) {
+        if (req.url.indexOf(path.dirname(operations[i].url)) === 0) {
+            return operations[i]
+        }
+    }
+    return null;
+}
+
+function respondWith(fakeDir, res, absPath) {
+    res.setHeader('Content-Type', fakeDir.contentType)
+    fs.readFile(absPath, function (err, data) {
+        if (err) {
+            // Error could be that the user is looking at a dir.
+            // Servers have index files for that.
+            if (err.code !== 'EISDIR' || !fakeDir.indexFile) {
+                return onIOError(res, err);
+            } else {
+                var index = fakeDir.indexFile;
+                index = path.join(absPath, index)
+                return respondWith(fakeDir, res, index)
+            }
+        }
+
+        res.end(data)
+    })
+}
+
+function onIOError(res, err) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.statusCode = 404
+    res.write('MagicProxy fakeDir IO error');
+    console.error(err);
+    res.write(err.toString());
+    return res.end()
+}
